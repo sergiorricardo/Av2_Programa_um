50d291cdd2069fceea6c39c42603e08ba2e8545a
dani barros <danskylive@gmail.com>
2017-06-02 16:00:46 -0300

    troca para tela principal
    alteração para abrir tela principal

--- Av2-Trabalho/Program.cs
+++ Av2-Trabalho/Program.cs
@@ -16,7 +16,7 @@ namespace Av2_Trabalho
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form3());
+            Application.Run(new Form1());
         }
     }
 }
